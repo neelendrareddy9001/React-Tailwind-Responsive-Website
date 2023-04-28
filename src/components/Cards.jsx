754a109f691aa339618480d63f0ae9cb5cf21d86
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-29 00:38:18 +0200

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -18,6 +18,28 @@ const Cards = () => {
                 </div>
                 <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
             </div>
+            <div className='w-fulll shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                <img className='w-28 mx-auto mt-[-3rem] bg-white' src={Double} alt='/'/>
+                <h2 className='text-2xl font-bold text-center py-8' >Double User</h2>
+                <p className='text-center text-4xl font-bold'>$149</p>
+                <div className='text-center font-medium'>
+                    <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
+                    <p className='py-2 border-b mx-8'>a Granted User</p>
+                    <p className='py-2 border-b mx-8'>Sed up to 2 GB</p>
+                </div>
+                <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+            </div>
+            <div className='w-fulll shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                <img className='w-28 mx-auto mt-[-3rem] bg-white' src={Triple} alt='/'/>
+                <h2 className='text-2xl font-bold text-center py-8' >Triple User</h2>
+                <p className='text-center text-4xl font-bold'>$149</p>
+                <div className='text-center font-medium'>
+                    <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
+                    <p className='py-2 border-b mx-8'>a Granted User</p>
+                    <p className='py-2 border-b mx-8'>Sed up to 2 GB</p>
+                </div>
+                <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+            </div>
         </div>
     </div>
   )
