b389ea57221b2c3e9d5cc0f6d0bed265fa3060ca
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-28 22:39:15 +0200

    Add files via upload

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Newsletter = () => {
+  return (
+    <div className='w-full py-16 text-white'>
+        <div className='max-w-[1240px] mx-auto grid lg-grid-cols-3'>
+            <div className='lg:col-span-2'>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Want trips & ticks to optimize your flow?</h1>
+                <p>Sign up to our newsletter and stay up to date.</p>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Newsletter
