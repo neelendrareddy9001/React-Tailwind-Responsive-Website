809726b2cb9dfb90fc94851f1f8fef2817e3816e
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-24 23:33:09 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,11 +24,11 @@ const Navbar = () => {
         <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
           <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
           <ul className='p-4 uppercase'>
-            <li className='p-4 border-b border-gray-600'>Home</li>
-            <li className='p-4 border-b border-gray-600'>Company</li>
-            <li className='p-4 border-b border-gray-600'>Resourses</li>
-            <li className='p-4 border-b border-gray-600'>About</li>
-            <li className='p-4 border-b border-gray-600'>Contact</li>
+            <li className='p-4 border-b border-gray-600'><Link to='hero'></Link>Home</li>
+            <li className='p-4 border-b border-gray-600'><Link to='analytics'></Link>Company</li>
+            <li className='p-4 border-b border-gray-600'><Link to='newsletter'></Link>Resourses</li>
+            <li className='p-4 border-b border-gray-600'><Link to='cards'></Link>About</li>
+            <li className='p-4 border-b border-gray-600'><Link to='footer'></Link>Contact</li>
           </ul>
         </div>
     </div>
