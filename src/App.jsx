9639bf2e11e8dbe6288c9e7bda363fc4d902d9a7
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-07-12 17:42:29 +0200

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+//import Router 
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Analytics from './components/Analytics';
-import Newsletter from './components/Newsletter';
-import Cards from './components/Cards';
-import Footer from './components/Footer';
 
+import AnimateRoutes from './components/AnimateRoutes';
 
 function App () {
   return (
-    <div>
-      <Routes>
-        <Route path='/' exact element={<Navbar/>}/>
-        <Route path='/hero' exact element={<Hero/>}/>
-        <Route path='/analytics' exact element={<Analytics/>}/>
-        <Route path='/newsletter' exact element={<Newsletter/>}/>
-        <Route path='/cards' exact element={<Cards/>}/>
-        <Route path='/footer' exact element={<Footer/>}/>
-      </Routes>
-      {/* <Navbar/>
-      <Hero/>
-      <Analytics/>
-      <Newsletter />
-      <Cards/>
-      <Footer/> */}
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <AnimateRoutes/>
+      </Router>
+    </>
   )
 }
 
---
+++ src/components/AnimateRoutes.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+//import pages
+import Hero from './Hero';
+import Cards from './Cards';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import Newsletter from './Newsletter';
+import Analytics from './Analytics';
+
+//import Routes and Route & useLocation hook
+import {Routes, Route, useLocation} from 'react-router-dom';
+
+const AnimateRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence>
+    <Routes key={location.pathname} location={location}>
+      <Route path='/' element={<Hero/>}/>
+      <Route path='/analytics' element={<Analytics/>}/>
+      <Route path='/newsletter' element={<Newsletter/>}/>
+      <Route path='/cards' element={<Cards/>}/>
+      <Route path='/footer' element={<Footer/>} />
+    </Routes>
+    </AnimatePresence>
+  )
+}
+
+export default AnimateRoutes
